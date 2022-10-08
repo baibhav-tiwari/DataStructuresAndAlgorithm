18ec39adf855e0340b4f4403ff9a8d2d10214abc
Baibhav Tiwari <baibhavtiwari37@gmail.com>
2022-10-08 18:33:16 +0530

    reverse_array.cpp

--- array/reversearry.cpp
+++ array/reversearry.cpp
@@ -1,25 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
- void reverseArray(int arr[],int start,int end)
+using nameSpace std;
+int main(){
+ int t;
+ cin>>t;
+ while(t)
  {
-    while(start < end)
-    {
-        int temp=arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
-    }
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {
+   cin>>arr[i];
+  }
+  for(int i=n-1;i>=0;i++)
+  {
+   cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+  t--;
  }
- void printArray(int arr[],int size)
- {
-    for(int i=0;i<size;++i)
-    cout<<arr[i]<<"";
-    cout<<endl;
- }
- int main()
- {
-    int array[5];
-    cout<<"enter 5 elements of array:"<<endl;
-    for(int i=0;i<5;i++)
- }
+}
