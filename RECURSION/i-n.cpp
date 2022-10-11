ad7e05fab10d697f43918cbf9f68997588fe7c53
Baibhav Tiwari <baibhavtiwari37@gmail.com>
2022-10-11 21:51:39 +0530

    commit

--- RECURSION/i-n.cpp
+++ RECURSION/i-n.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-
 using namespace std;
-
 void printer(int n)
 {
     if (n == 0)
         return;
-
-    cout << n << " ";
     printer(n - 1);
+    cout << n << " ";
 }
-
 int main()
 {
-
     int n = 4;
     printer(n);
-
     return 0;
 }
