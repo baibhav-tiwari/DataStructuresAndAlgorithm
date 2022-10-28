8df3306827b3b02f8ccf0e17aad0051fd864ae15
Baibhav Tiwari <baibhavtiwari37@gmail.com>
2022-10-28 19:27:51 +0530

    commit

--- Array/largestelement.cpp
+++ Array/largestelement.cpp
@@ -1,4 +1,4 @@
-// Sorting approach
+// Sorting approach -> time complexity: O(N*log(N)) -> space complexity: O(N)
 #include <bits/stdc++.h>
 using namespace std;
 int SortArr(vector<int> &arr)
@@ -14,4 +14,31 @@ int main()
     cout << "largest element: " << SortArr(arr1) << endl;
     return 0;
 }
-// using a max variable
+
+// using a max variable -> Time Complexity: O(N) -> Space Complexity : O(1)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int LargestElement(int arr[], int n)
+{
+
+    int max = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int main()
+{
+    int arr[] = {5, 7, 9, 11, 14, 2};
+    int n = 6;
+    int max = LargestElement(arr, n);
+    cout << "The largest element in the array is: " << max << endl;
+
+    return 0;
+}
