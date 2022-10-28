c7684a2bde458fc5f4a54af3fb7e6fe8ed06aa0a
Baibhav Tiwari <baibhavtiwari37@gmail.com>
2022-10-28 21:08:19 +0530

    commit

--- Array/scnd_lrgst_smlst.cpp
+++ Array/scnd_lrgst_smlst.cpp
@@ -19,4 +19,35 @@ int main()
     return 0;
 }
 
-// using Traversal  -> time complexity
+// using Traversal  -> time complexity: O(N)
+#include <bits/stdc++.h>
+using namespace std;
+void getelement(int arr[], int n)
+{
+    if (n == 0 || n == 1)
+        cout << -1 << " " << -1 << endl;
+    int small = INT_MAX, second_small = INT_MAX;
+    int large = INT_MIN, second_large = INT_MIN;
+    int i;
+    for (int i = 0; i < n; i++)
+    {
+        small = min(small, arr[i]);
+        large = max(large, arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < second_small && arr[i] != small)
+            second_small = arr[i];
+        if (arr[i] > second_large && arr[i] != large)
+            second_large = arr[i];
+    }
+    cout << "second smallest: " << second_small << endl;
+    cout << "second largest: " << second_large << endl;
+}
+int main()
+{
+    int arr[] = {1, 24, 6, 7, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    getelement(arr, n);
+    return 0;
+}
